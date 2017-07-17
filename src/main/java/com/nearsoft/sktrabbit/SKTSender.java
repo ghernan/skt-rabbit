c804b6eb49ad540b256f2a227aaad35da3fcabe0
Antonio <ghernandez@nearsoft.com>
2017-07-17 13:17:58 -0600

    Added sender class.

---
+++ src/main/java/com/nearsoft/sktrabbit/SKTSender.java
@@ -0,0 +1,41 @@
+package com.nearsoft.sktrabbit;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by antoniohernandez on 7/17/17.
+ */
+public class SKTSender {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv)
+            throws java.io.IOException {
+
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            //factory.setPort(4030);
+            Connection connection =  factory.newConnection();
+            Channel channel = connection.createChannel();
+            
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+            channel.close();
+            connection.close();
+
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
