2c25f80747b4d6c97045ba7d731deecfd74ef49b
Antonio <ghernandez@nearsoft.com>
2017-07-17 15:29:49 -0600

    Port changed and receiver added.

--- src/main/java/com/nearsoft/sktrabbit/SKTSender.java
+++ src/main/java/com/nearsoft/sktrabbit/SKTSender.java
@@ -19,12 +19,13 @@ public class SKTSender {
         try {
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
-            //factory.setPort(4030);
+            factory.setPort(4030);
             Connection connection =  factory.newConnection();
+
             Channel channel = connection.createChannel();
-            
+
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
+            String message = "Hello World! "+ connection.getPort();
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
             channel.close();
