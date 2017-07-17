45dde86af4af07e8c45eeee5d8b461465f075012
Antonio <ghernandez@nearsoft.com>
2017-07-17 12:34:44 -0600

    initial commit, setup

---
+++ src/main/java/com/nearsoft/sktrabbit/SktRabbitApplication.java
@@ -0,0 +1,12 @@
+package com.nearsoft.sktrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SktRabbitApplication.class, args);
+	}
+}
